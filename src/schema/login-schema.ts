0ca6371c426536443b8f207bd01a21351a14bc82
nxhettry <nishan.gtsn@gmail.com>
2025-05-19 18:49:49 +0545

    created login schema using zod: :

---
+++ src/schema/login-schema.ts
@@ -0,0 +1,12 @@
+import z from "zod";
+
+export const loginFormSchema = z.object({
+  email: z
+    .string()
+    .email("Invalid email address")
+    .nonempty("Email is required"),
+  password: z
+    .string()
+    .min(6, "Password must be at least 6 characters long")
+    .nonempty("Password is required"),
+});
